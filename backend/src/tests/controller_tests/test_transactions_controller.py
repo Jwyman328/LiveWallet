052d0021cc96f2253b1143b94f79f34a127479c2
Joseph Wyman <jwymandev@gmail.com>
2024-10-20 11:55:30 -0400

    add test for get all outputs controller

--- backend/src/tests/controller_tests/test_transactions_controller.py
+++ backend/src/tests/controller_tests/test_transactions_controller.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
 
 from unittest.mock import MagicMock
+from typing import List
 from src.app import AppCreator
 from src.services.wallet.wallet import WalletService
 from src.tests.mocks import all_transactions_mock
 import json
+from bitcoinlib.transactions import Output
 
 
 class TestTransactionsController(TestCase):
@@ -28,4 +30,19 @@ class TestTransactionsController(TestCase):
             assert get_transactions_response.status == "200 OK"
             assert json.loads(get_transactions_response.data) == {
                 "transactions": [tx.as_dict() for tx in all_transactions_mock]
+            }
+
+    def test_get_utxos(self):
+        output_lists: List[List[Output]] = [tx.outputs for tx in all_transactions_mock]
+        all_outputs = [output for output_list in output_lists for output in output_list]
+        get_all_outputs_mock = MagicMock(return_value=all_outputs)
+        with self.app.container.wallet_service.override(self.mock_wallet_service):
+            self.mock_wallet_service.get_all_outputs = get_all_outputs_mock
+            get_all_outputs_response = self.test_client.get("transactions/outputs")
+
+            get_all_outputs_mock.assert_called_once()
+
+            assert get_all_outputs_response.status == "200 OK"
+            assert json.loads(get_all_outputs_response.data) == {
+                "outputs": [output.as_dict() for output in all_outputs]
             }
