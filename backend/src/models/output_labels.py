f49b950b50bcd84ff2e686b03bc5afbc534a386c
Joseph Wyman <jwymandev@gmail.com>
2024-11-17 18:59:35 -0500

    remove commented out code that will not be used

--- backend/src/models/output_labels.py
+++ backend/src/models/output_labels.py
@@ -1,19 +1,3 @@
-# from sqlalchemy import Table, ForeignKey, Integer
-#
-# from src.database import DB
-#
-#
-# class OutputLabel(DB.Model):
-#     __tablename__ = "output_labels"
-#     id = DB.Column(Integer, primary_key=True)
-#     output_id = DB.Column(
-#         "output_id", DB.String, ForeignKey("outputs.id"), primary_key=True
-#     )
-#     label_id = DB.Column(
-#         "label_id", DB.String, ForeignKey("labels.id"), primary_key=True
-#     )
-
-
 from sqlalchemy import Table, Column, String, ForeignKey
 from src.database import DB
 
